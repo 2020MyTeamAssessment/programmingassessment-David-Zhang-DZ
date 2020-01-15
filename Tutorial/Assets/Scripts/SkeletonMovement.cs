54c98b05c48bbfbce541ee8ae509083e95815f94
David-Zhang-DZ <57647832+David-Zhang-DZ@users.noreply.github.com>
2020-01-15 00:09:27 -0600

    SkeletonAnimationShooting
    Skeletons can now shoot arrows and die after shooting three arrows.

--- Tutorial/Assets/Scripts/SkeletonMovement.cs
+++ Tutorial/Assets/Scripts/SkeletonMovement.cs
@@ -6,20 +6,21 @@ public class SkeletonMovement : MonoBehaviour
 {
     Rigidbody2D skeletonRB;
     public Rigidbody2D projectilePrefab;
-    int timer;
+    public Animator animator;
+    int counter;
     float speed = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        int timer = 1;
+        counter = 0;
         skeletonRB = gameObject.GetComponent<Rigidbody2D>();
+        StartCoroutine(SkeletonMove());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     void Jump()
@@ -30,19 +31,56 @@ public class SkeletonMovement : MonoBehaviour
     
     void ShootArrow()
     {
+ 
+        StartCoroutine(ShootArrowTask());
+        counter++;
+        Debug.Log(counter);
+    }
+
+    IEnumerator ShootArrowTask()
+    {
+        animator.SetBool("IsShooting", true);
+        yield return new WaitForSeconds(1f);
         Vector2 movement = new Vector2(2.0f, 0f);
         Vector2 position = new Vector2(transform.position.x, transform.position.y);
         Rigidbody2D projectile = Instantiate(projectilePrefab, position, Quaternion.identity);
         projectile.velocity = movement;
+        animator.SetBool("IsShooting", false);
+
     }
 
     private void FixedUpdate()
     {
-        if ((timer % 150) == 1)
-        {
-            ShootArrow();
-            Jump();
-        }
-        timer++;
+        
     }
+
+    IEnumerator SkeletonMove()
+    {
+        for (; ; )
+        {
+            
+            Jump();
+            yield return new WaitForSeconds(2);
+            ShootArrow();
+            yield return new WaitForSeconds(2);
+            if (counter > 2)
+            {
+                animator.SetTrigger("Death");
+                yield return new WaitForSeconds(1.5f);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    IEnumerator PlayDeadAnimation()
+    {
+        if(counter > 2)
+        {
+            animator.SetBool("IsDead", true);
+            yield return new WaitForSeconds(.98f);
+            Destroy(gameObject);
+        }
+        yield return new WaitForSeconds(0);
+    }
+
 }
