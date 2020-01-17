865216c8017c84a65394d9d95ac451220bfb2910
David-Zhang-DZ <57647832+David-Zhang-DZ@users.noreply.github.com>
2020-01-16 20:31:44 -0600

    SkeletonMovement
    Removed unnecessary components, made code more clear

--- Tutorial/Assets/Scripts/SkeletonMovement.cs
+++ Tutorial/Assets/Scripts/SkeletonMovement.cs
@@ -7,13 +7,14 @@ public class SkeletonMovement : MonoBehaviour
     Rigidbody2D skeletonRB;
     public Rigidbody2D projectilePrefab;
     public Animator animator;
-    int counter;
-    float speed = 3.0f;
+    int arrowsShot;
+    float jumpForce = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        counter = 0;
+        //
+        arrowsShot = 0;
         skeletonRB = gameObject.GetComponent<Rigidbody2D>();
         StartCoroutine(SkeletonMove());
     }
@@ -23,47 +24,48 @@ public class SkeletonMovement : MonoBehaviour
     {
     }
 
+    //Cause skeleton to jump
     void Jump()
     {
-        Vector2 movement = new Vector2(0f, 5.0f);
+        Vector2 movement = new Vector2(0f, jumpForce);
         skeletonRB.AddForce(movement, ForceMode2D.Impulse);
     }
-    
+
+    //Helper function for shooting with time delay
     void ShootArrow()
     {
- 
+
         StartCoroutine(ShootArrowTask());
-        counter++;
-        Debug.Log(counter);
+        arrowsShot++;
     }
 
+    //Play the shooting animation and creates another arrow 
     IEnumerator ShootArrowTask()
     {
         animator.SetBool("IsShooting", true);
         yield return new WaitForSeconds(1f);
+
         Vector2 movement = new Vector2(2.0f, 0f);
         Vector2 position = new Vector2(transform.position.x, transform.position.y);
         Rigidbody2D projectile = Instantiate(projectilePrefab, position, Quaternion.identity);
         projectile.velocity = movement;
+
         animator.SetBool("IsShooting", false);
 
     }
 
-    private void FixedUpdate()
-    {
-        
-    }
 
+    //contains all of the actions performed by the skeleton
     IEnumerator SkeletonMove()
     {
         for (; ; )
         {
-            
+
             Jump();
             yield return new WaitForSeconds(2);
             ShootArrow();
             yield return new WaitForSeconds(2);
-            if (counter > 2)
+            if (arrowsShot > 2)
             {
                 animator.SetTrigger("Death");
                 yield return new WaitForSeconds(1.5f);
@@ -71,16 +73,5 @@ public class SkeletonMovement : MonoBehaviour
             }
         }
     }
-
-    IEnumerator PlayDeadAnimation()
-    {
-        if(counter > 2)
-        {
-            animator.SetBool("IsDead", true);
-            yield return new WaitForSeconds(.98f);
-            Destroy(gameObject);
-        }
-        yield return new WaitForSeconds(0);
-    }
-
 }
+ 
